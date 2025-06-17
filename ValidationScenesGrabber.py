80bc17934b5cc13cfab5c9189eca72f8437c056e
Shihabyooo <43100312+Shihabyooo@users.noreply.github.com>
2025-06-17 21:30:24 +0400

    Modified code to allow export of best n images per month, instead of only one. Added date to export file name.

--- ValidationScenesGrabber.py
+++ ValidationScenesGrabber.py
@@ -21,6 +21,9 @@ months : list[int] = [6, 7, 8, 9, 10, 11]
 distToClip : int = 1000
 splitOutput : bool = False
 
+maxImagesPerMonth = 3 #Must be greater than 0. Will prioritize least cloudy images. Will be capped to available dates.
+#i.e. if maxImagesPerMonth = 1, then only the least cloudy image will be exported. if 2: least and second least. And so on.
+
 #================================================
 #Processing start
 #================================================
@@ -34,83 +37,85 @@ roi = roi.map(lambda circle : ee.Geometry.Rectangle(ee.Array.cat(circle.geometry
 
 for month in months:
         #pre filtering the images
-        col = sentinel2SR.filterDate(f"{year}-{month}-01", f"{year}-{month + 1}-1" if month < 12 else  f"{year + 1}-1-1").filterBounds(roi.geometry())
+        col : ee.ImageCollection = sentinel2SR.filterDate(f"{year}-{month}-01", f"{year}-{month + 1}-1" if month < 12 else  f"{year + 1}-1-1").filterBounds(roi.geometry())
         col = col.select(['B4', 'B3', 'B2', 'B8'])
+        #add a formatted date property (makes our life easier)
+        col = col.map(lambda img : img.set({"date" : ee.Date(img.get("system:time_start")).format("y-M-d")}))
 
-        #generate a single composite/mosaic of the entire roi, using the least cloudy images for the month
-        #First, get a list of MRGS tiles covering the points.
-        tileList = col.distinct("MGRS_TILE").aggregate_array("MGRS_TILE").getInfo()
+        #mosaic the images in a single date into one image
+        dates = col.aggregate_array("date").distinct().getInfo()
+        monthImages = ee.List([])
 
-        #Note: Sentinel-2 Data aren't always produced as perfect rectangles fitting an MGRS tiles. Some tiles are covered by the triangular/trapizoilda remainder of a sentine scene
-        #after clipping neighbouring tiles. So, we do an internal mosaic for this tile first (inside the loop bellow), before doing the mosaicing over all tiles
-        bestScences = ee.List([])
+        for date in dates:
+                subCol = col.filter(ee.Filter.eq("date", date))
+                meanCloudyPercentage = subCol.aggregate_array("CLOUDY_PIXEL_PERCENTAGE").reduce(ee.Reducer.mean())
+                monthImages = monthImages.add(subCol.mosaic().set({"date" : date, "CLOUDY_PIXEL_PERCENTAGE" : meanCloudyPercentage}))
         
-        for tileID in tileList:
-                #mosaic puts last image on top, so you want your least cloudy image as the last one.
-                tileCol = col.filter(ee.Filter.eq("MGRS_TILE", tileID)).sort("CLOUDY_PIXEL_PERCENTAGE", False)
-                tileImg = tileCol.mosaic()
-                bestScences = bestScences.add(tileImg)
+        #convert to an image collection, then sort based on cloud percentage.
+        monthImages = ee.ImageCollection(monthImages).sort("CLOUDY_PIXEL_PERCENTAGE")
+        #convert back to list because we need to iterate through them in order
+        monthImages = monthImages.toList(monthImages.size())
+        
+        for i in range (0, min(maxImagesPerMonth, monthImages.size().getInfo())):
+                targetImageLarge = ee.Image(monthImages.get(i)).clip(roi.geometry())
 
-        bestScences = ee.ImageCollection(bestScences)
-        targetImageLarge = bestScences.mosaic().clip(roi.geometry()).toInt16()
-
-        if not splitOutput:
-                outputFileName = f"RGB_{year}_{month}"
-
-                print ("exporting: ", outputFileName)
-
-                task = ee.batch.Export.image.toDrive(
-                                                        image = targetImageLarge,
-                                                        description = outputFileName,
-                                                        #folder='ee_export',
-                                                        #region = quadrants[i],
-                                                        scale = 10,
-                                                        crs = 'EPSG:4326',
-                                                        maxPixels = 3e10,
-                                                        fileFormat = 'GeoTIFF',
-                                                        formatOptions = {
-                                                                'noData': 0
-                                                        })      
-                task.start()
-
-        else:
-                #To split the image into four, first, compute the bounds for splitting (quardants)
-                boundingBox = ee.Array.cat(roi.geometry().bounds().coordinates(), 1)
-
-                xCoords = boundingBox.slice(1, 0, 1)
-                yCoords = boundingBox.slice(1, 1, 2)
-                xMin = xCoords.reduce('min', [0]).get([0,0])
-                xMax = xCoords.reduce('max', [0]).get([0,0])
-                yMin = yCoords.reduce('min', [0]).get([0,0])
-                yMax = yCoords.reduce('max', [0]).get([0,0])
-
-                xHalf = xMin.add(xMax).divide(2.0)
-                yHalf = yMin.add(yMax).divide(2.0)
-
-                #ee.Geometry.BBox(west, south, east, north)
-                quadrants = []
-                quadrants.append(ee.Geometry.BBox(xMin, yMin, xHalf, yHalf))
-                quadrants.append(ee.Geometry.BBox(xHalf, yMin, xMax, yHalf))
-                quadrants.append(ee.Geometry.BBox(xHalf, yHalf, xMax, yMax))
-                quadrants.append(ee.Geometry.BBox(xMin, yHalf, xHalf, yMax))
-
-                #export
-                for i in range (0, 4):
-                        outputFileName = f"RGB_{year}_{month}_part_{i}"
+                if not splitOutput:
+                        outputFileName = f"RGBNir_{targetImageLarge.get("date").getInfo()}"
 
                         print ("exporting: ", outputFileName)
-                        targetImage = targetImageLarge.clip(quadrants[i])
 
                         task = ee.batch.Export.image.toDrive(
-                        image = targetImage,
-                        description = outputFileName,
-                        #folder='ee_export',
-                        region = quadrants[i],
-                        scale = 10,
-                        crs = 'EPSG:4326',
-                        maxPixels = 200000000,
-                        fileFormat = 'GeoTIFF',
-                        formatOptions = {
-                                'noData': 0
-                        })
+                                                                image = targetImageLarge,
+                                                                description = outputFileName,
+                                                                #folder='ee_export',
+                                                                #region = quadrants[i],
+                                                                scale = 10,
+                                                                crs = 'EPSG:4326',
+                                                                maxPixels = 3e10,
+                                                                fileFormat = 'GeoTIFF',
+                                                                formatOptions = {
+                                                                        'noData': 0
+                                                                })      
                         task.start()
+
+                else:
+                        #To split the image into four, first, compute the bounds for splitting (quardants)
+                        boundingBox = ee.Array.cat(roi.geometry().bounds().coordinates(), 1)
+
+                        xCoords = boundingBox.slice(1, 0, 1)
+                        yCoords = boundingBox.slice(1, 1, 2)
+                        xMin = xCoords.reduce('min', [0]).get([0,0])
+                        xMax = xCoords.reduce('max', [0]).get([0,0])
+                        yMin = yCoords.reduce('min', [0]).get([0,0])
+                        yMax = yCoords.reduce('max', [0]).get([0,0])
+
+                        xHalf = xMin.add(xMax).divide(2.0)
+                        yHalf = yMin.add(yMax).divide(2.0)
+
+                        #ee.Geometry.BBox(west, south, east, north)
+                        quadrants = []
+                        quadrants.append(ee.Geometry.BBox(xMin, yMin, xHalf, yHalf))
+                        quadrants.append(ee.Geometry.BBox(xHalf, yMin, xMax, yHalf))
+                        quadrants.append(ee.Geometry.BBox(xHalf, yHalf, xMax, yMax))
+                        quadrants.append(ee.Geometry.BBox(xMin, yHalf, xHalf, yMax))
+
+                        #export
+                        for i in range (0, 4):
+                                outputFileName = f"RGBNir_{targetImageLarge.get("date").getInfo()}_part_{i}"
+
+                                print ("exporting: ", outputFileName)
+                                targetImage = targetImageLarge.clip(quadrants[i])
+
+                                task = ee.batch.Export.image.toDrive(
+                                image = targetImage,
+                                description = outputFileName,
+                                #folder='ee_export',
+                                region = quadrants[i],
+                                scale = 10,
+                                crs = 'EPSG:4326',
+                                maxPixels = 200000000,
+                                fileFormat = 'GeoTIFF',
+                                formatOptions = {
+                                        'noData': 0
+                                })
+                                task.start()
